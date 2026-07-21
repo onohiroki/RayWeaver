897a3fa44ed92b784b351907ba8b1294abbd1824
ONO Hiroki <onohiroki@mac.com>
2026-07-21 16:11:44 +0900

    Regenerate sample outputs with A4 landscape SVG scaling

--- samples/run-demo.bash
+++ samples/run-demo.bash
@@ -69,7 +69,7 @@ echo 'with chief data:'
 
 echo
 echo "=== SVG raytrace diagram ==="
-./rayweave chief --clear-aperture --marginal-rays < "$YAML" \
+./rayweave chief --clear-aperture < "$YAML" | ./rayweave chief --marginal-rays \
   | ./rayweave trace \
   | ./rayweave plot -o "$OUTDIR/us2645157.svg"
 echo "Written: $OUTDIR/us2645157.svg"
